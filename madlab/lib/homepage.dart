daddcf76b26d783fbe1cf7cce12997c29c2ac938
Simar Kaur <89771491+simarkaur28@users.noreply.github.com>
2023-04-25 21:34:42 +0530

    feat: Win Count Line Graphs

--- madlab/lib/homepage.dart
+++ madlab/lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:madlab/cricapi.dart';
 import 'package:madlab/profilepage.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({required Key key}) : super(key: key);
@@ -13,55 +14,185 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home page"),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Menu',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
+        appBar: AppBar(
+          title: Text("Home page"),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text(
+                  'Menu',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
                 ),
               ),
-            ),
-            ListTile(
-              title: Text('My Profile'),
-              onTap: () {
-                Navigator.pop(context); // close the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => profile()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Route 2'),
-              onTap: () {
-                Navigator.pop(context); // close the drawer
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MatchesList()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Route 3'),
-              onTap: () {
-                Navigator.pop(context); // close the drawer
-                Navigator.pushNamed(context, '/route3');
-              },
-            ),
-          ],
+              ListTile(
+                title: Text('My Profile'),
+                onTap: () {
+                  Navigator.pop(context); // close the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => profile()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Route 2'),
+                onTap: () {
+                  Navigator.pop(context); // close the drawer
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MatchesList()),
+                  );
+                },
+              ),
+              ListTile(
+                title: Text('Route 3'),
+                onTap: () {
+                  Navigator.pop(context); // close the drawer
+                  Navigator.pushNamed(context, '/route3');
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(
+                    text:
+                        'Season Feature \n IPL Team Performance Statistic (Win Count): \nSeason 2017'),
+                // Enable legend
+                legend: Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_Matches, String>>[
+                  LineSeries<_Matches, String>(
+                      dataSource: <_Matches>[
+                        _Matches('SRH', 8),
+                        _Matches('MI', 12),
+                        _Matches('GL', 4),
+                        _Matches('RPS', 10),
+                        _Matches('RCB', 3),
+                        _Matches('KKR', 9),
+                        _Matches('DD', 6),
+                        _Matches('KXIP', 7),
+                        _Matches('CSK', 0),
+                        _Matches('RR', 0),
+                        _Matches('SRH1', 0)
+                      ],
+                      xValueMapper: (_Matches WinCount, _) => WinCount.team,
+                      yValueMapper: (_Matches WinCount, _) => WinCount.WinCount,
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+            SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(
+                    text:
+                        'IPL Team Performance Statistic (Win Count): \nSeason 2014'),
+                // Enable legend
+                legend: Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_Matches, String>>[
+                  LineSeries<_Matches, String>(
+                      dataSource: <_Matches>[
+                        _Matches('SRH', 4),
+                        _Matches('MI', 3),
+                        _Matches('GL', 0),
+                        _Matches('RPS', 0),
+                        _Matches('RCB', 5),
+                        _Matches('KKR', 6),
+                        _Matches('DD', 2),
+                        _Matches('KXIP', 8),
+                        _Matches('CSK', 8),
+                        _Matches('RR', 7),
+                        _Matches('SRH1', 0)
+                      ],
+                      xValueMapper: (_Matches WinCount, _) => WinCount.team,
+                      yValueMapper: (_Matches WinCount, _) => WinCount.WinCount,
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+            SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(
+                    text:
+                        'IPL Team Performance Statistic (Win Count):\n Season 2013'),
+                // Enable legend
+                legend: Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_Matches, String>>[
+                  LineSeries<_Matches, String>(
+                      dataSource: <_Matches>[
+                        _Matches('SRH', 10),
+                        _Matches('MI', 13),
+                        _Matches('GL', 0),
+                        _Matches('RPS', 4),
+                        _Matches('RCB', 9),
+                        _Matches('KKR', 6),
+                        _Matches('DD', 3),
+                        _Matches('KXIP', 8),
+                        _Matches('CSK', 12),
+                        _Matches('RR', 11),
+                        _Matches('SRH1', 0)
+                      ],
+                      xValueMapper: (_Matches WinCount, _) => WinCount.team,
+                      yValueMapper: (_Matches WinCount, _) => WinCount.WinCount,
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ]),
+            SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                title: ChartTitle(
+                    text:
+                        'IPL Team Performance Statistic (Win Count):\n Season 2012'),
+                // Enable legend
+                legend: Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_Matches, String>>[
+                  LineSeries<_Matches, String>(
+                      dataSource: <_Matches>[
+                        _Matches('SRH', 4),
+                        _Matches('MI', 10),
+                        _Matches('GL', 0),
+                        _Matches('RPS', 4),
+                        _Matches('RCB', 8),
+                        _Matches('KKR', 12),
+                        _Matches('DD', 11),
+                        _Matches('KXIP', 8),
+                        _Matches('CSK', 10),
+                        _Matches('RR', 7),
+                        _Matches('SRH1', 0)
+                      ],
+                      xValueMapper: (_Matches WinCount, _) => WinCount.team,
+                      yValueMapper: (_Matches WinCount, _) => WinCount.WinCount,
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(isVisible: true))
+                ])
+          ]),
+        ));
   }
+}
+
+class _Matches {
+  _Matches(this.team, this.WinCount);
+
+  final String team;
+  final double WinCount;
 }
